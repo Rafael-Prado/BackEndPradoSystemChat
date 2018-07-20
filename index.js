6170f34a2cc9db8e19f28acc17f826ef51705bd4
Rafael-Prado <rafaeldacruzdoprado@hotmail.com>
2018-07-20 15:03:10 -0300

    backEnd PradoSystemChat

---
+++ index.js
@@ -0,0 +1,25 @@
+let app = require('express')();
+let server = require('http').Server(app);
+let io = require('socket.io').listen(server);
+
+
+io.on('connection', (socket) =>{
+    socket.on('disconnect', function(){
+        io.emit('user-changed', {user: socket.pradoname, event: 'left'});
+    });
+
+    socket.on('set-pradoname', (pradoname) =>{
+        socket.pradoname = pradoname;
+        io.emit('user-changed', {user: socket.pradoname, event: 'joined'});
+    });
+
+    socket.on('add-message', (message) =>{
+        io.emit('message', {text: message.text, from: socket.pradoname, created: new Date()});
+    });
+});
+
+let port = process.env.port ||3001;
+
+server.listen(port, function(){
+        console.log('listening in http://localhost:' + port);
+});
